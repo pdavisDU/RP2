212ce9b6477f3a3fdd00b8f84fc630cc1a42ba3c
pdavisDU <pdavisdu@gmail.com>
2023-04-11 11:19:38 -0500

    mapping over UL and showing item names

---
+++ src/components/List.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function List({ groceries }) {
+  return (
+    <ul className="list-group">
+      {/* TODO: Use the `map` method to iterate over each grocery and return a `li` element that contains the item's name and has a unique key attribute */}
+      {groceries.map((item) => {
+        return (
+          // TODO: Your code here
+          <li key={item.name}>{`${item.name}`}</li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default List;
