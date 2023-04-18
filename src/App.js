8e0be1f58b6cc52838786a6e251a6fc9ed21b0d0
pdavisDU <pdavisdu@gmail.com>
2023-04-18 11:12:14 -0500

    adding new src

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-// First we import Greeting from the components folder
-import Greeting from "./components/Greeting";
+import Display from "./components/Display";
 
-// Our App component returns the Greeting component
 function App() {
-  return <Greeting />;
+  return <Display />;
 }
 
 export default App;
