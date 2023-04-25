facfa77129ceda9ab79e04159edafca9107dc8e4
pdavisDU <pdavisdu@gmail.com>
2023-04-25 16:49:12 -0500

    adding new practice src folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-// First we import our Alert component from the components folder
-import Alert from './components/Alert';
+import List from './components/List';
+import Nav from './components/Nav';
 
-// We create a couple variables which contain strings
-const message = 'Invalid user id or password';
-const alertType = "danger"
+// Import our list of users from users.js
+import users from './users';
 
-// We then return Alert, we pass it a prop, "message" set to the value of our message variable
-// We also pass a type prop to the component set to the value of "alertType"
-function App() {
-  return <Alert type={alertType} message={message} />;
+// Pass users array to the List component as a prop
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <List users={users} />
+    </div>
+  );
 }
-
-export default App;
